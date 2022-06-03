b63f1a7b542070082d37a5db347bb6d0a510ab5d
LuisCarlox <luish77302@gmail.com>
2022-06-02 23:06:00 -0600

    Mas ejercicios agregados
    [Ticket: 2]
    Se agregaron mas ejercicios resueltos.
    
    ".git/COMMIT_EDITMSG" 14L, 297C

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(list) {
+    let nuevoarreglo = [];
+    for(let i=0;i<list.length;i++){
+        nuevoarreglo.push(list[i]["title"]);
+    }
+    return nuevoarreglo;
 };
 
 // Do not edit below this line
